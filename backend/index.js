447c7a715c2c766c96581d405e4d0e7f7388cbd4
Minh Lam <ltm1701@gmail.com>
2021-04-18 20:24:38 -0600

    backend\index file with middlewares

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const uploadRoutes = require("./routes/api/uploads");
+app.use("*", cors());
+app.use(express.json());
+app.use(uploadRoutes);
+app.listen(8000);
