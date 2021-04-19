607dc502e5773f861e5c72f0c4ef37b5864512fd
Minh Lam <ltm1701@gmail.com>
2021-04-19 08:12:02 -0600

    backend\add handler for response check

--- backend/index.js
+++ backend/index.js
@@ -5,4 +5,9 @@ const uploadRoutes = require("./routes/api/uploads");
 app.use("*", cors());
 app.use(express.json());
 app.use(uploadRoutes);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
 app.listen(8000);
